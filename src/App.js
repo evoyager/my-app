7dde0d625ec232ede26a3231e74a13e4ab3a0e5e
Ievgen Gusar <ievgen_gusar@epam.com>
2019-06-13 00:58:15 +0300

    Added Prism support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Prism from "prismjs"
 import logo from './logo.svg';
 import './App.css';
+import './prism.css'
 
 function App() {
+  var test = "HELLO WORLD!!!";
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <pre><code className="language-css">{test}</code></pre>
         <a
           className="App-link"
           href="https://reactjs.org"
